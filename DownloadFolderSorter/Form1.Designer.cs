2b6f5be7dbd39ac120a45133f6bf8507a6b0c4a9
Niklas Carstensen <creeper99.mc@gmail.com>
2018-12-16 01:09:39 +0100

    Projektdateien hinzufügen.

---
+++ DownloadFolderSorter/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace DownloadFolderSorter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.OperationName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FileNameMatching = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ToFolder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bApply = new System.Windows.Forms.Button();
+            this.lStatus = new System.Windows.Forms.Label();
+            this.tFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bBrowser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.OperationName,
+            this.FileNameMatching,
+            this.ToFolder});
+            this.dataGrid.Enabled = false;
+            this.dataGrid.Location = new System.Drawing.Point(13, 41);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(609, 195);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // OperationName
+            // 
+            this.OperationName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.OperationName.HeaderText = "Name";
+            this.OperationName.Name = "OperationName";
+            this.OperationName.Width = 60;
+            // 
+            // FileNameMatching
+            // 
+            this.FileNameMatching.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.FileNameMatching.HeaderText = "File Name Matching";
+            this.FileNameMatching.Name = "FileNameMatching";
+            this.FileNameMatching.Width = 115;
+            // 
+            // ToFolder
+            // 
+            this.ToFolder.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ToFolder.HeaderText = "Target Folder";
+            this.ToFolder.Name = "ToFolder";
+            // 
+            // bApply
+            // 
+            this.bApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bApply.Enabled = false;
+            this.bApply.Location = new System.Drawing.Point(547, 242);
+            this.bApply.Name = "bApply";
+            this.bApply.Size = new System.Drawing.Size(75, 23);
+            this.bApply.TabIndex = 1;
+            this.bApply.Text = "Apply";
+            this.bApply.UseVisualStyleBackColor = true;
+            this.bApply.Click += new System.EventHandler(this.bApply_Click);
+            // 
+            // lStatus
+            // 
+            this.lStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lStatus.AutoSize = true;
+            this.lStatus.Enabled = false;
+            this.lStatus.Location = new System.Drawing.Point(12, 247);
+            this.lStatus.Name = "lStatus";
+            this.lStatus.Size = new System.Drawing.Size(43, 13);
+            this.lStatus.TabIndex = 2;
+            this.lStatus.Text = "Status: ";
+            // 
+            // tFolder
+            // 
+            this.tFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tFolder.Location = new System.Drawing.Point(108, 14);
+            this.tFolder.Name = "tFolder";
+            this.tFolder.Size = new System.Drawing.Size(409, 20);
+            this.tFolder.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Download Folder:";
+            // 
+            // bBrowser
+            // 
+            this.bBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bBrowser.Location = new System.Drawing.Point(523, 12);
+            this.bBrowser.Name = "bBrowser";
+            this.bBrowser.Size = new System.Drawing.Size(99, 23);
+            this.bBrowser.TabIndex = 6;
+            this.bBrowser.Text = "Browse Folders";
+            this.bBrowser.UseVisualStyleBackColor = true;
+            this.bBrowser.Click += new System.EventHandler(this.bBrowser_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 277);
+            this.Controls.Add(this.bBrowser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tFolder);
+            this.Controls.Add(this.lStatus);
+            this.Controls.Add(this.bApply);
+            this.Controls.Add(this.dataGrid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(450, 250);
+            this.Name = "MainForm";
+            this.Text = "Download Folder Sorter";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button bApply;
+        private System.Windows.Forms.Label lStatus;
+        private System.Windows.Forms.TextBox tFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OperationName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileNameMatching;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ToFolder;
+        private System.Windows.Forms.Button bBrowser;
+    }
+}
+
---
+++ DownloadFolderSorter/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadFolderSorter
+{
+    public partial class MainForm : Form
+    {
+        FileSystemWatcher weightwatchers;
+        bool dfolderExists;
+        bool isSortable;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            weightwatchers = new FileSystemWatcher();
+            setDownloadFolder(config.Default.downloadFolder);
+            loadDataGrid();
+            ShowWindow(this.Handle, 2);
+        }
+
+        private void setDownloadFolder(string folder)
+        {
+            dfolderExists = false;
+
+            tFolder.Text = folder;
+            config.Default.downloadFolder = folder;
+            config.Default.Save();
+
+            dfolderExists = Directory.Exists(folder);
+            dataGrid.Enabled = dfolderExists;
+            bApply.Enabled = dfolderExists;
+            lStatus.Enabled = dfolderExists;
+
+            if (!dfolderExists)
+                lStatus.Text = "Status: Got no valid Download-Folder!";
+            else
+            {
+                lStatus.Text = "Status: Ready";
+
+                weightwatchers.Path = folder;
+                weightwatchers.Changed += ((object source, FileSystemEventArgs e) =>
+                {
+                    try
+                    {
+                        SortDownloadFolder();
+                    } catch (Exception ex) { MessageBox.Show("Sorting Error: \n" + e.ToString()); }
+                });
+                weightwatchers.EnableRaisingEvents = true;
+
+                SortDownloadFolder();
+            }
+        }
+        private void bBrowser_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.SelectedPath = tFolder.Text;
+            dialog.Description = "Select your Download-Folder";
+            if (dialog.ShowDialog() == DialogResult.OK)
+                setDownloadFolder(dialog.SelectedPath);
+        }
+        
+        private void bApply_Click(object sender, EventArgs e)
+        {
+            int rows = dataGrid.Rows.Count - 1;
+            config.Default.names = new string[rows];
+            config.Default.fileMatches = new string[rows];
+            config.Default.targetFolders = new string[rows];
+            
+            for (int i = 0; i < rows; i++)
+            {
+                config.Default.names[i] = (string)dataGrid.Rows[i].Cells[0].Value;
+                config.Default.fileMatches[i] = (string)dataGrid.Rows[i].Cells[1].Value;
+                config.Default.targetFolders[i] = (string)dataGrid.Rows[i].Cells[2].Value;
+            }
+
+            config.Default.Save();
+
+            if (!CanSort())
+                lStatus.InvokeIfRequired(() => { lStatus.Text = "Status: Error, current configuration is invalid, check the input matrix"; });
+
+            SortDownloadFolder();
+        }
+        private void loadDataGrid()
+        {
+            if (config.Default.names == null || config.Default.fileMatches == null || config.Default.targetFolders == null)
+                return;
+
+            int rows = config.Default.targetFolders.Length;
+
+            for (int i = 0; i < rows; i++)
+                dataGrid.Rows.Add(new object[] { config.Default.names[i], config.Default.fileMatches[i], config.Default.targetFolders[i] });
+        }
+        private bool CanSort()
+        {
+            if (!dfolderExists || !Directory.Exists(config.Default.downloadFolder) ||
+                config.Default.targetFolders == null || config.Default.names == null || config.Default.fileMatches == null ||
+                config.Default.targetFolders.Length == 0 || config.Default.names.Length != config.Default.targetFolders.Length || 
+                config.Default.fileMatches.Length != config.Default.targetFolders.Length)
+                return false;
+
+            for (int i = 0; i < config.Default.targetFolders.Length; i++)
+                if (!Directory.Exists(config.Default.targetFolders[i]))
+                    return false;
+
+            return true;
+        }
+        private void SortDownloadFolder()
+        {
+            if (!CanSort())
+                return;
+
+            lStatus.InvokeIfRequired(() => { lStatus.Text = "Status: Sorting..."; });
+            string[] files = Directory.GetFiles(config.Default.downloadFolder);
+            for (int i = 0; i < files.Length; i++)
+                for (int j = 0; j < config.Default.fileMatches.Length; j++)
+                {
+                    string[] split = config.Default.fileMatches[j].Split('|');
+                    for (int k = 0; k < split.Length; k++)
+                        if (Path.GetFileName(files[i]).Contains(split[k]) && !File.Exists(config.Default.targetFolders[j] + "\\" + Path.GetFileName(files[i])))
+                            File.Move(files[i], config.Default.targetFolders[j] + "\\" + Path.GetFileName(files[i]));
+                }
+            lStatus.InvokeIfRequired(() => { lStatus.Text = "Status: Ready"; });
+        }
+
+        // dll Imports
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    }
+
+    public static class Extensions
+    {
+        public static void InvokeIfRequired(this ISynchronizeInvoke obj, MethodInvoker action)
+        {
+            if (obj.InvokeRequired)
+            {
+                var args = new object[0];
+                obj.Invoke(action, args);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
